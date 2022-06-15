720c07a900d4b1ebd11f5747e7961fc2ff6be2f0
maurosj77 <mauroluna77@hotmail.com>
2022-06-15 01:58:52 -0300

    .

--- Javascript/ejercicios/codigo.js
+++ Javascript/ejercicios/codigo.js
@@ -1,11 +1,1 @@
-function addElement () {
-    // obtener el elemento div con id = "div_example"
-    const existDiv = document.getElementById("div_example");
-    // crear un nuevo elemento div
-    const newDiv = document.createElement("div");
-    // agregar el nuevo elemento div existente
-    existDiv.appendChild(newDiv);
-   }
-   
-
-   document.write("kaka");
+let mensaje = document.getElementById('maik');
